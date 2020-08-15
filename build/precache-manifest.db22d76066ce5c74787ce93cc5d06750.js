a34869f786395a95d4546ff3bcc2fce8dcf07090
Septian Rifano <septianinet@gmail.com>
2020-08-16 01:28:23 +0700

    build

---
+++ build/precache-manifest.db22d76066ce5c74787ce93cc5d06750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19b0580d9cdc77fa0c604104a13b239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bff45e8a5a612e9f163",
+    "url": "/static/css/main.dbbb0f12.chunk.css"
+  },
+  {
+    "revision": "81285e14036b3b1bd522",
+    "url": "/static/js/2.580ac685.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.580ac685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bff45e8a5a612e9f163",
+    "url": "/static/js/main.e0251122.chunk.js"
+  },
+  {
+    "revision": "2cd32b02ed4e2ab1eb25",
+    "url": "/static/js/runtime-main.c611b372.js"
+  }
+]);
